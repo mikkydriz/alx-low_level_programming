5018f712a330474aefed64c76e2a137182688e57
mikkydriz <michaelksam30@gmail.com>
2022-12-12 15:26:44 -0800

    code fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main- Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,9 +9,9 @@ int main(void)
 
 {
 
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+		return (0);
+
 
 
 }
